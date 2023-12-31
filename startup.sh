7a796685090705a7ce4015367f0970f935d47060
rk0775 <118426413+rk0775@users.noreply.github.com>
2023-12-31 14:52:57 +0530

    add install_startup file

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-MARKETPLACE_PATH=$PWD
+MARKETPLACE_PATH=/app/marketplace-main
 echo marketplace path : $MARKETPLACE_PATH
 
 # kill all process
