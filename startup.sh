b6cd1d487ff7fb9c102629153ab722c9c65ce10f
rk0775 <118426413+rk0775@users.noreply.github.com>
2024-01-03 21:11:14 +0530

    sudo su command add in startup file

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+sudo su
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
 systemctl restart mysql
 systemctl status mysql
